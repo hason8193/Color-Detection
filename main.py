74c885cc79770ff672d4f5f9fa290f84f8437b9f
Trần Hà Sơn <111623729+hason8193@users.noreply.github.com>
2024-03-16 09:35:01 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+import cv2
+import numpy as np
+from PIL import Image
+def get_limits(color):
+    c = np.uint8([[color]])  
+    hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
+    hue = hsvC[0][0][0] 
+    if hue >= 165:  
+        lowerLimit = np.array([hue - 10, 100, 100], dtype=np.uint8)
+        upperLimit = np.array([180, 255, 255], dtype=np.uint8)
+    elif hue <= 15:  
+        lowerLimit = np.array([0, 100, 100], dtype=np.uint8)
+        upperLimit = np.array([hue + 10, 255, 255], dtype=np.uint8)
+    else:
+        lowerLimit = np.array([hue - 10, 100, 100], dtype=np.uint8)
+        upperLimit = np.array([hue + 10, 255, 255], dtype=np.uint8)
+    return lowerLimit, upperLimit
+
+black = [0,0,0]
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+
+    hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lowerLimit, upperLimit = get_limits(color=black)
+
+    mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
+
+    mask_ = Image.fromarray(mask)
+
+    bbox = mask_.getbbox()
+
+    if bbox is not None:
+        x1, y1, x2, y2 = bbox
+
+        frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
+
+    cv2.imshow('frame', mask)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+
+cv2.destroyAllWindows()
